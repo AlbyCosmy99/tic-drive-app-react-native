caa0a51c385b3bbea32dad50cfd657067dedf6d7
Andrei <dinamo1999@icloud.com>
2024-08-25 23:59:23 +0200

    changes

--- components/WorkshopCards.tsx
+++ components/WorkshopCards.tsx
@@ -1,19 +1,27 @@
 import { View } from "react-native";
 import WorkshopCard from "./WorkshopCard";
-import { ScrollView } from "react-native-gesture-handler";
-import workshops from '../constants/temp/Workshops'
+import { ScrollView, TouchableOpacity } from "react-native-gesture-handler";
+import workshops, { Workshop } from '../constants/temp/Workshops'
 import GlobalContext from "@/app/stateManagement/contexts/GlobalContext";
 import { useContext, useEffect } from "react";
 
 function WorkshopCards() {
     const {workshopFilter, setWorkshopFilter} = useContext(GlobalContext)
 
+    const handleCardPress = (workshop: Workshop) => {
+        alert('pressed ' + workshop.title)
+    }
+
     return (
         <ScrollView>
             {workshops
                 .filter(workshop => workshopFilter.length === 0 || workshop.title.toLowerCase().includes(workshopFilter.toLowerCase().trim()))
                 .map((workshop, index) => {
-                return <WorkshopCard workshop={workshop} key={index}/>
+                return (
+                    <TouchableOpacity key={index} onPress={() => handleCardPress(workshop)}>
+                        <WorkshopCard workshop={workshop}/>
+                    </TouchableOpacity>
+                )
             })}
         </ScrollView>
     )
