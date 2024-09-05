3fb5493d44d55fa93f597214617b8824247dc983
Andrei <dinamo1999@icloud.com>
2024-09-06 00:36:14 +0200

    changes

--- app/screens/RegisterVehicle.tsx
+++ app/screens/RegisterVehicle.tsx
@@ -46,19 +46,19 @@ function RegisterVehicle() {
 
     return (
         <SafeAreaView style={styles.safeArea} className="flex-1">
-            <TouchableOpacity onPress={() => router.back()} style={styles.backButton}>
+            <TouchableOpacity onPress={() => router.back()} className="m-2 mb-7">
                   <Ionicons name="arrow-back" size={30} color="#000" />
             </TouchableOpacity>
             <View className="flex-1 justify-between">
                 <Text className="font-medium mb-2 text-3xl mx-3.5">Register your vehicle for service bookings</Text>
                 <SegmentedControl segmentedControlSelection={segmentedControlSelection} setSegmentedControlSelection={setSegmentedControlSelection} />
-                <View style={styles.bookingDetailsContainer} className="flex-1 m-3.5">
+                <View style={styles.bookingDetailsContainer} className="flex-1 m-3.5 border-2 rounded-xl">
                     {
                         options.map((option,index) => {
                             return segmentedControlSelection.name === option.name &&
                                 (
                                     <View key={index}>
-                                        <Text style={styles.plateNumberLabel} key={index}>{option.inputLabel}</Text>
+                                        <Text className="font-semibold mx-3.5 mt-3.5 mb-0 text-lg" key={index}>{option.inputLabel}</Text>
                                         <TicDriveInput 
                                             placeholder={option.placeholder} 
                                             isRightIcon={true} 
@@ -66,7 +66,7 @@ function RegisterVehicle() {
                                             option={option.keyString}
                                             setIsCarSearched={setIsCarSearched}
                                         />
-                                        {carNotFound && isCarSearched && <Text style={styles.carNotFound}>Car not found. Try again.</Text>}
+                                        {carNotFound && isCarSearched && <Text className="text-base mx-auto text-red-600">Car not found. Try again.</Text>}
                                     </View>
                                     
                                 )
@@ -75,34 +75,34 @@ function RegisterVehicle() {
                     <ScrollView>
                         {
                             carSelected.id > 0 && (
-                                <View style={styles.carDetailsContainer}>
-                                    <View style={styles.carDetailContainer}>
-                                        <Text style={[styles.carLabel]}>Model</Text>
-                                        <Text style={styles.carDetail}>{carSelected.model}</Text>
+                                <View className="mx-3.5">
+                                    <View className="my-1 border-b" style={styles.carDetailContainer}>
+                                        <Text className="font-bold mb-0.5 text-lg">Model</Text>
+                                        <Text className="text-lg mb-1.5">{carSelected.model}</Text>
                                     </View>
-                                    <View style={styles.carDetailContainer}>
-                                        <Text style={[styles.carLabel]}>Plate number</Text>
-                                        <Text style={styles.carDetail}>{carSelected.plateNumber}</Text>
+                                    <View className="my-1 border-b" style={styles.carDetailContainer}>
+                                        <Text className="font-bold mb-0.5 text-lg">Plate number</Text>
+                                        <Text className="text-lg mb-1.5">{carSelected.plateNumber}</Text>
                                     </View>
-                                    <View style={styles.carDetailContainer}>
-                                        <Text style={[styles.carLabel]}>VIN</Text>
-                                        <Text style={styles.carDetail}>{carSelected.vin}</Text>
+                                    <View className="my-1 border-b" style={styles.carDetailContainer}>
+                                        <Text className="font-bold mb-0.5 text-lg">VIN</Text>
+                                        <Text className="text-lg mb-1.5">{carSelected.vin}</Text>
                                     </View>
-                                    <View style={styles.carDetailContainer}>
-                                        <Text style={[styles.carLabel]}>Liters</Text>
-                                        <Text style={styles.carDetail}>{carSelected.liters}</Text>
+                                    <View className="my-1 border-b" style={styles.carDetailContainer}>
+                                        <Text className="font-bold mb-0.5 text-lg">Liters</Text>
+                                        <Text className="text-lg mb-1.5">{carSelected.liters}</Text>
                                     </View>
-                                    <View style={styles.carDetailContainer}>
-                                        <Text style={[styles.carLabel]}>Energy</Text>
-                                        <Text style={styles.carDetail}>{carSelected.energy}</Text>
+                                    <View className="my-1 border-b" style={styles.carDetailContainer}>
+                                        <Text className="font-bold mb-0.5 text-lg">Energy</Text>
+                                        <Text className="text-lg mb-1.5">{carSelected.energy}</Text>
                                     </View>
-                                    <View style={styles.carDetailContainer}>
-                                        <Text style={[styles.carLabel]}>Engine code</Text>
-                                        <Text style={styles.carDetail}>{carSelected.engineCode}</Text>
+                                    <View className="my-1 border-b" style={styles.carDetailContainer}>
+                                        <Text className="font-bold mb-0.5 text-lg">Engine code</Text>
+                                        <Text className="text-lg mb-1.5">{carSelected.engineCode}</Text>
                                     </View>
-                                    <View style={styles.carDetailContainer}>
-                                        <Text style={[styles.carLabel]}>Engine displacement (cc)</Text>
-                                        <Text style={styles.carDetail}>{carSelected.engineDisplacement}</Text>
+                                    <View className="my-1 border-b" style={styles.carDetailContainer}>
+                                        <Text className="font-bold mb-0.5 text-lg">Engine displacement (cc)</Text>
+                                        <Text className="text-lg mb-1.5">{carSelected.engineDisplacement}</Text>
                                     </View>
                                 </View>
                             )
@@ -119,46 +119,13 @@ const styles = StyleSheet.create({
     safeArea: {
         backgroundColor: Colors.light.background,
     },
-    backButton: {
-        margin: 8,
-        marginBottom: 30,
-    },
     bookingDetailsContainer: {
         backgroundColor: Colors.light.background,
         borderColor: Colors.light.SegmentedControlBackground,
-        borderWidth: 2,
-        borderRadius: 10,
-    },
-    plateNumberLabel: {
-        fontSize: 16,
-        fontWeight: '600',
-        marginHorizontal: 15,
-        marginTop: 15,
-        marginBottom: 0
-    },
-    carDetailsContainer: {
-        marginHorizontal: 15
-    },
-    carDetail: {
-        fontSize: 18,
-        marginBottom:5
-    },
-    carLabel: {
-        fontWeight: '700',
-        fontSize: 18,
-        marginBottom: 2
     },
     carDetailContainer: {
-        marginBottom: 3,
-        borderBottomWidth: 1,
         borderBottomColor: Colors.light.SegmentedControlBackground,
-        marginTop: 3
     },
-    carNotFound: {
-        marginHorizontal: 'auto',
-        fontSize: 16,
-        color: 'red'
-    }
 });
 
 export default RegisterVehicle;
