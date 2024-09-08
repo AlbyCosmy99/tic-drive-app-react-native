923363938f760c76cf5324f3415f6bc30bf51579
Andrei <dinamo1999@icloud.com>
2024-09-08 10:03:38 +0200

    changes

--- app/(nav)/_layout.tsx
+++ app/(nav)/_layout.tsx
@@ -15,16 +15,16 @@ export default function Nav() {
                 colors={[Colors.light.backgroundLinearGradient.start, Colors.light.backgroundLinearGradient.end]}
                 className="flex-1 absolute w-full h-full"
             />
-            <SafeAreaView className="flex-1">
-                <TicDriveNavbar />
-                <View className="flex-1 justify-between">
-                    <Text style={{ color: colorScheme === 'light' ? Colors.light.text : Colors.dark.text }} className="font-medium text-3xl mx-3.5 mb-2">
-                        What service are you looking for?
-                    </Text>
-                    <ServicesCards />
-                </View>
-                <TicDriveButton text="Book a service" path="/screens/RegisterVehicle" />
-            </SafeAreaView>
+                <SafeAreaView className="flex-1">
+                    <TicDriveNavbar />
+                    <View className="flex-1 justify-between">
+                        <Text style={{ color: colorScheme === 'light' ? Colors.light.text : Colors.dark.text }} className="font-medium text-3xl mx-3.5 mb-2">
+                            What service are you looking for?
+                        </Text>
+                        <ServicesCards />
+                    </View>
+                    <TicDriveButton text="Book a service" path="/screens/RegisterVehicle" />
+                </SafeAreaView>
         </View>
     );
 }
