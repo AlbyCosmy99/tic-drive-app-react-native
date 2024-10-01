b96aa94c0999d40a0d862c3a8538b9bf0ab8310b
Andrei <dinamo1999@icloud.com>
2024-10-01 16:05:26 +0200

    fix: iphone inset notch

--- app/utils/isIphoneWithHomeIndicator.ts
+++ app/utils/isIphoneWithHomeIndicator.ts
@@ -1,5 +1,37 @@
-import DeviceInfo from 'react-native-device-info'
+import * as Device from 'expo-device';
 
-const isIphoneWithHomeIndicator = () => DeviceInfo.hasNotch()
+const hasNotchDevices = [
+    'iPhone X',
+    'iPhone XR',
+    'iPhone XS',
+    'iPhone XS Max',
+    'iPhone 11',
+    'iPhone 11 Pro',
+    'iPhone 11 Pro Max',
+    'iPhone 12',
+    'iPhone 12 Mini',
+    'iPhone 12 Pro',
+    'iPhone 12 Pro Max',
+    'iPhone 13',
+    'iPhone 13 Mini',
+    'iPhone 13 Pro',
+    'iPhone 13 Pro Max',
+    'iPhone 14',
+    'iPhone 14 Plus',
+    'iPhone 14 Pro',
+    'iPhone 14 Pro Max',
+    'iPhone 15',
+    'iPhone 15 Plus',
+    'iPhone 15 Pro',
+    'iPhone 15 Pro Max',
+  ];
+  
 
-export default isIphoneWithHomeIndicator
+const isIphoneWithHomeIndicator = () => {
+  if (Device.brand === 'Apple' && hasNotchDevices.includes(Device.modelName)) {
+    return true;
+  }
+  return false;
+};
+
+export default isIphoneWithHomeIndicator;
