fe84d44b962e0854bafd14c892449d19e6b5d13e
Andrei <dinamo1999@icloud.com>
2024-10-04 14:45:40 +0200

    feat: implement login button in welcome page

--- app/(hub)/_layout.tsx
+++ app/(hub)/_layout.tsx
@@ -1,32 +1,59 @@
-import TicDriveNavbar from "@/components/navigation/TicDriveNavbar";
 import { LinearGradient } from "expo-linear-gradient";
-import { router } from "expo-router";
-import { Pressable, StyleSheet, Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { router, useNavigation } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";;
 import { globalStyles } from "../globalStyles";
 import { Colors } from "@/constants/Colors";
-import necessaryDeviceBottomInset from "../utils/necessaryDeviceBottomInset";
 import { Image } from "@rneui/themed";
 import TicDriveLogo from '../../assets/images/TicDriveLogo.jpeg';
 import ServicesCard from "@/components/ServicesCard";
+import LottieView from 'lottie-react-native'
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { useAppDispatch, useAppSelector } from "../stateManagement/redux/hooks";
+import { logout } from "../stateManagement/redux/slices/authSlice";
+import { saveLoginStatus } from "../utils";
+import { Entypo } from "@expo/vector-icons";
+import { StackActions } from "@react-navigation/native";
 
 const ChooseUserModeScreen = () => {
+
+    const navigation = useNavigation()
+    const dispatch = useAppDispatch()
+    const isUserLogged = useAppSelector((state) => state.auth.isAuthenticated)
+
+    const handleLogout = async () => {
+        dispatch(logout())
+        await saveLoginStatus(false)
+        //sostituire con redux thunk?
+      }
+    
+
     return (
-        <View className={`flex-1 ${necessaryDeviceBottomInset()}`}>
+        <View className={`flex-1`}>
             <LinearGradient
                 colors={[Colors.light.backgroundLinearGradient.start,Colors.light.backgroundLinearGradient.start, Colors.light.green.drive]}
                 locations={[0, 0.45, 1]} 
                 className="flex-1 absolute w-full h-full"
-            />
-                <SafeAreaView className="flex-1" style={globalStyles.safeAreaView}>
-                    <View className="flex-1 justify-between">
-                        <View className="justify-center items-center flex-1">
-                            <Image 
-                                source={TicDriveLogo}
-                                style={styles.logoImage}
-                            />
-                        </View>
-                        <View style={styles.content} className="flex-row bg-green-500 justify-center items-center p-2">
+            >
+                <View className="flex-1 justify-between" style={globalStyles.safeAreaView}>
+                    <View className="justify-center items-center flex-1">
+                        <Image 
+                            source={TicDriveLogo}
+                            style={styles.logoImage}
+                        />
+                    </View>
+                    <LottieView
+                        source={require('@/assets/json/animations/carPhoneSliding.json')}
+                        autoPlay
+                        loop
+                        style={styles.lottieAnimation}
+                    />
+                    <LinearGradient
+                        colors={[Colors.light.backgroundLinearGradient.start, Colors.light.green.drive, Colors.light.green.drive]} // Define your gradient colors here
+                        style={styles.content} // Apply gradient to the content view
+                        locations={[0, 0.4, 1]} // Adjust the position of the gradient transition
+                        className="p-2 items-end"
+                    >
+                        <View className="flex-row justify-center items-center p-2">
                             <View className="flex-1">
                                 <ServicesCard 
                                     id={1} 
@@ -43,7 +70,7 @@ const ChooseUserModeScreen = () => {
                             </View>
                             <View className="flex-1">
                                 <ServicesCard 
-                                    id={1} 
+                                    id={2} 
                                     title="Offer a service" 
                                     description="What services do you want to offer?"
                                     cardStyle={styles.card}
@@ -56,8 +83,32 @@ const ChooseUserModeScreen = () => {
                                 />
                             </View>
                         </View>
-                    </View>
-                </SafeAreaView>
+                        <View className='flex-1 justify-end flex-row mx-5'>
+                            {isUserLogged ? (
+                                    <TouchableOpacity onPress={handleLogout} className='p-2.5'>
+                                    <View className='flex-row gap-1 items-center justify-center'>
+                                        <Entypo name="login" size={24} color={Colors.light.text} />
+                                        <Text className='text-xl' style={styles.login}>Logout</Text>
+                                    </View>
+                                    </TouchableOpacity>
+                                ) : (
+                                    <TouchableOpacity onPress={() => {
+                                    if(navigation.canGoBack()) {
+                                        navigation.dispatch(StackActions.popToTop());
+                                    }
+                                    router.push('../screens/UserAuthentification')
+                                    }} className='p-2.5 pl-0 flex-row gap-2'>
+                                    <View className='flex-row gap-1 items-center justify-center'>
+                                        <Entypo name="login" size={24} color={Colors.light.text} />
+                                        <Text className='text-xl' style={styles.login}>Login</Text>
+                                    </View>
+                                    </TouchableOpacity>
+                                )
+                            }
+                        </View>
+                    </LinearGradient>
+                </View>
+            </LinearGradient>
         </View>
     );
 }
@@ -66,7 +117,7 @@ const styles = StyleSheet.create({
     content: {
         borderTopLeftRadius: 35,
         borderTopRightRadius: 35,
-        height: 280
+        height: 280,
     },
     hr: {
         borderBottomColor: '#e6e6e6',
@@ -103,7 +154,15 @@ const styles = StyleSheet.create({
     card: {
         backgroundColor: 'lightgreen',
         borderColor: Colors.light.green.drive
-    }
+    },
+    lottieAnimation: {
+        width: '100%',
+        alignSelf: 'flex-end',
+        height: 250,
+    },
+    login: {
+        color: Colors.light.text,  
+    },
 });
 
 
