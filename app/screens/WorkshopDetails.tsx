0aa01b8ae81b56f38a90ba06ae1498f342a63edb
Andrei <dinamo1999@icloud.com>
2024-10-06 21:36:48 +0200

    fix: workshopDetails improvements when workshop is not found

--- app/screens/WorkshopDetails.tsx
+++ app/screens/WorkshopDetails.tsx
@@ -6,15 +6,15 @@ import { useEffect, useState } from "react";
 import { ActivityIndicator, SafeAreaView, StyleSheet, Text, View } from "react-native";
 import { ScrollView, TouchableOpacity } from "react-native-gesture-handler";
 import Icon from "react-native-vector-icons/FontAwesome";
-import LocationPin from '../../assets/svg/location_on.svg';
-import Verified from '../../assets/svg/verified.svg';
-import Star from '../../assets/svg/star.svg';
-import Acute from '../../assets/svg/acute.svg';
-import FreeCancellation from '../../assets/svg/free_cancellation.svg';
-import AssistantDirection from '../../assets/svg/assistant_direction';
-import CalendarIcon from '../../assets/svg/calendar_add_on.svg';
+import LocationPin from '../../assets/svg/location_on.svg'
+import Verified from '../../assets/svg/verified.svg'
+import Star from '../../assets/svg/star.svg'
+import Acute from '../../assets/svg/acute.svg'
+import FreeCancellation from '../../assets/svg/free_cancellation.svg'
+import AssistantDirection from '../../assets/svg/assistant_direction'
+import CalendarIcon from '../../assets/svg/calendar_add_on.svg'
 import Review from "@/constants/temp/Review";
-import ChatIcon from '../../assets/svg/chat.svg';
+import ChatIcon from '../../assets/svg/chat.svg'
 import { Ionicons } from "@expo/vector-icons";
 import TicDriveButton from "@/components/ui/buttons/TicDriveButton";
 import ClientReviewCards from "@/components/ClientReviewCards";
@@ -23,8 +23,7 @@ import necessaryDeviceBottomInset from "../utils/necessaryDeviceBottomInset";
 
 export default function WorkshopDetails() {
     const { id } = useLocalSearchParams();
-    
-    // Hooks are always called consistently
+
     const [workshop, setWorkshop] = useState<Workshop | null>(null);
 
     useEffect(() => {
@@ -32,50 +31,55 @@ export default function WorkshopDetails() {
     }, [id]);
 
     const calculateDiscountPrice = (price: string, discount: number) => {
-        const priceValue = parseInt(price.slice(1));
-        return priceValue - (priceValue * discount) / 100;
-    };
+        const priceValue = parseInt(price.slice(1))
+        return priceValue - priceValue*discount/100
+    }
 
     const calculateWorkshopStars = (reviews: Review[]) => {
-        let sumReviewStars = 0;
-        reviews.forEach((review) => {
-            sumReviewStars += review.stars;
-        });
-        return sumReviewStars / reviews.length;
-    };
+        let sumReviewStars = 0
+        reviews.forEach(review => {
+            sumReviewStars += review.stars
+        })
+        return sumReviewStars/ reviews.length
+    }
 
     return (
         <SafeAreaView style={[styles.container, globalStyles().safeAreaView]} className={`flex-1 p-2.5 ${necessaryDeviceBottomInset()}`}>
             <View className="flex-row items-center justify-between mr-2.5">
-                <TouchableOpacity
-                    onPress={() => router.back()}
+                <TouchableOpacity 
+                    onPress={() => router.back()} 
                     className="m-2 mb-2.5"
                     accessible={true}
                     accessibilityLabel="Back to previous page"
                 >
                     <Ionicons name="arrow-back" size={30} color="#000" />
                 </TouchableOpacity>
-                <Text className="font-bold text-lg">Workshop</Text>
-                <View>
-                    {workshop?.favourite ? (
-                        <Icon name="heart" size={30} color="red" />
-                    ) : (
-                        <Icon name="heart" size={30} color="white" />
-                    )}
-                </View>
-            </View>
-
-            <ScrollView>
-                <View style={styles.container} className="flex-1 p-2.5">
-                    {!workshop ? (
-                        <View className="flex-1 justify-center items-center">
-                            <Text className="text-red-600 text-xl">Workshop not found.</Text>
-                        </View>
-                    ) : (
+                {
+                    workshop && (
                         <>
+                            <Text className="font-bold text-lg">Workshop</Text>
+                            <View>
+                                {
+                                    workshop.favourite ? (
+                                        <Icon name="heart" size={30} color="red" />
+                                    ) : (<Icon name="heart" size={30} color="white" />)
+                                }
+                            </View>
+                        </>
+                    )
+                }
+            </View>
+            {!workshop ? (
+                <View className="flex-1 justify-center items-center">
+                    <Text className="text-red-600 text-xl">Workshop not found.</Text>
+                </View>
+            ) : (
+                <>
+                    <ScrollView>
+                        <View style={styles.container} className="flex-1 p-2.5">
                             <View className="w-full relative border-b-2" style={styles.cardContainer}>
                                 <Image
-                                    source={{ uri: workshop.imageUrl }}
+                                    source={{uri: workshop.imageUrl}}
                                     containerStyle={styles.image}
                                     PlaceholderContent={<ActivityIndicator size="large" color={Colors.light.bookingsOptionsText} />}
                                 />
@@ -85,41 +89,39 @@ export default function WorkshopDetails() {
                                 </View>
                                 <View className="flex-1 flex-row mt-2.5">
                                     <View className="flex-1 flex-row items-center gap-0.5">
-                                        <Acute width={24} name="acute" />
+                                        <Acute width={24} name="acute"/>
                                         <Text className="text-lg">Express service</Text>
                                     </View>
                                     {workshop.freeCancellation && (
                                         <View className="flex-1 flex-row items-center gap-0.5">
-                                            <FreeCancellation width={24} name="acute" />
+                                            <FreeCancellation width={24} name="acute"/>
                                             <Text className="text-lg">Free cancellation</Text>
                                         </View>
                                     )}
                                 </View>
                                 <View className="flex-1 flex-row gap-2.5 mt-2.5 mb-3.5 flex-wrap">
-                                    <TouchableOpacity
-                                        className="flex-row items-center gap-0.5 border px-2.5 py-1.5 rounded-3xl"
-                                        style={styles.cardOptionContainer}
-                                        onPress={() => alert('directions')}
+                                    <TouchableOpacity 
+                                        className="flex-row items-center gap-0.5 border px-2.5 py-1.5 rounded-3xl" 
+                                        style={styles.cardOptionContainer} onPress={() => alert('directions')}
                                         accessible={true}
                                         accessibilityLabel="Map directions"
                                     >
                                         <AssistantDirection width={24} />
                                         <Text className="font-medium text-base">Directions</Text>
                                     </TouchableOpacity>
-                                    <TouchableOpacity
-                                        className="flex-row items-center gap-0.5 border px-2.5 py-1.5 rounded-3xl"
-                                        style={styles.cardOptionContainer}
-                                        onPress={() => alert('check availability')}
+                                    <TouchableOpacity 
+                                        className="flex-row items-center gap-0.5 border px-2.5 py-1.5 rounded-3xl" 
+                                        style={styles.cardOptionContainer} onPress={()=> alert('check availability')}
                                         accessible={true}
                                         accessibilityLabel="Check availability"
                                     >
                                         <CalendarIcon width={24} />
                                         <Text className="font-medium text-base">Check availability</Text>
                                     </TouchableOpacity>
-                                    <TouchableOpacity
-                                        className="flex-row items-center gap-0.5 border px-2.5 py-1.5 rounded-3xl"
-                                        style={styles.cardOptionContainer}
-                                        onPress={() => alert('message')}
+                                    <TouchableOpacity 
+                                        className="flex-row items-center gap-0.5 border px-2.5 py-1.5 rounded-3xl" 
+                                        style={styles.cardOptionContainer} 
+                                        onPress={()=> alert('message')}
                                         accessible={true}
                                         accessibilityLabel="Message"
                                     >
@@ -128,46 +130,43 @@ export default function WorkshopDetails() {
                                     </TouchableOpacity>
                                 </View>
                             </View>
-
                             <View className="mt-2.5">
                                 <Text className="text-xl font-bold">Location</Text>
                                 <View className="flex-1 flex-row items-center gap-0.5 mt-2.5">
-                                    <LocationPin width={24} name="location-pin" fill={Colors.light.ticText} />
+                                    <LocationPin width={24} name="location-pin" fill={Colors.light.ticText}/>
                                     <Text className="text-lg">{workshop.position}</Text>
                                 </View>
                             </View>
-
                             <View className="mt-2.5">
                                 <Text className="text-xl font-bold">What people say</Text>
                                 <View className="flex-1 flex-row items-center gap-0.5 mt-2.5">
-                                    <Star width={24} name="location-pin" fill={Colors.light.ticText} />
+                                    <Star width={24} name="location-pin" fill={Colors.light.ticText}/>
                                     <Text className="text-lg">{calculateWorkshopStars(workshop.reviews)} ({workshop.reviews.length} reviews)</Text>
                                 </View>
-                                <ClientReviewCards id={Array.isArray(id) ? id[0] : id} />
+                                <ClientReviewCards id={Array.isArray(id) ? id[0] : id}/>
                             </View>
-                            <View style={styles.bottom} className="flex-row justify-between items-center mx-2.5 border-t">
-                                <View className="flex-1 flex-col mt-2.5">
-                                    <Text className="text-lg" style={styles.startingFrom}>Starting from</Text>
-                                    <View className="flex-row items-center">
-                                        <View>
-                                            <Text className={[workshop?.discount !== 0 ? 'text-red-500' : '', 'font-bold text-2xl mx-1'].join(' ')}>{workshop?.price}</Text>
-                                            {workshop?.discount !== 0 && <View style={styles.strikethroughLine} />}
-                                        </View>
-                                        {workshop?.discount !== 0 && (
-                                            <Text className="font-bold text-2xl mx-1">
-                                                ${calculateDiscountPrice(workshop?.price ?? "0", workshop?.discount ?? 0)}
-                                            </Text>
-                                        )}
-                                    </View>
-                                </View>
+                        </View>
+                    </ScrollView>
+                    <View style={styles.bottom} className="flex-row justify-between items-center mx-2.5 border-t">
+                        <View className="flex-1 flex-col mt-2.5">
+                            <Text className="text-lg" style={styles.startingFrom}>Starting from</Text>
+                            <View className="flex-row items-center">
                                 <View>
-                                    <TicDriveButton text="Book a service" path={'../screens/CalendarDateSelection'} customButtonStyle={styles.customButtonStyle} />
+                                    <Text className={[
+                                            workshop.discount !== 0 ? 'text-red-500' : '',
+                                            'font-bold text-2xl mx-1'
+                                        ].join(' ')}>{workshop.price}</Text>
+                                    {workshop.discount !==0 && <View style={styles.strikethroughLine} />}
                                 </View>
+                                {workshop.discount !== 0 && <Text className="font-bold text-2xl mx-1">${calculateDiscountPrice(workshop.price, workshop.discount)}</Text>}
                             </View>
-                        </>
-                    )}
-                </View>
-            </ScrollView>
+                        </View>
+                        <View>
+                            <TicDriveButton text="Book a service" path={'../screens/CalendarDateSelection'} customButtonStyle={styles.customButtonStyle}/>
+                        </View>
+                    </View>
+                </>
+            )}
         </SafeAreaView>
     );
 }
@@ -177,7 +176,7 @@ const styles = StyleSheet.create({
         backgroundColor: Colors.light.background,
     },
     bottom: {
-        borderTopColor: Colors.light.SegmentedControlBackground,
+        borderTopColor: Colors.light.SegmentedControlBackground
     },
     image: {
         width: '100%',
@@ -203,6 +202,6 @@ const styles = StyleSheet.create({
         color: Colors.light.placeholderText,
     },
     customButtonStyle: {
-        height: 50,
+        height: 50
     },
 });
