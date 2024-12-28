01ccdacd6baf79ceb0f102c8156aa959e1d0a75d
Andrei Albu <dinamo1999@icloud.com>
2024-12-28 12:11:53 +0100

    feat: use axios instead of fetch

--- services/auth/login.ts
+++ services/auth/login.ts
@@ -1,24 +1,14 @@
 import User from '@/types/User';
+import axiosClient from '../http/axiosClient';
 
 export const login = async (user: User): Promise<any> => {
     try {
-        const response = await fetch("https://ticdrive20241221234140.azurewebsites.net/api/auth/login", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                email: user.email,
-                password: user.password,
-            })
-        });
+        const response = await axiosClient.post('auth/login', {
+            email: user.email,
+            password: user.password,
+        });        
 
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        const data = await response.json();
-        return data;
+        return response.data
     } catch (err) {
         throw err;
     }
--- services/auth/register.ts
+++ services/auth/register.ts
@@ -1,27 +1,16 @@
 import User from '@/types/User';
+import axiosClient from '../http/axiosClient';
 
 const register = async (user: User): Promise<any> => {
     try {
-        const response = await fetch("https://ticdrive20241221234140.azurewebsites.net/api/auth/register", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                name: user.name,
-                email: user.email,
-                password: user.password,
-                confirmPassword: user.repeatedPassword,
-                userType: user.category === 'user' ? 1 : 2
-            })
-        });
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        const data = await response.json();
-        return data;
+        const response =await axiosClient.post('auth/register', {
+            name: user.name,
+            email: user.email,
+            password: user.password,
+            confirmPassword: user.repeatedPassword,
+            userType: user.category === 'user' ? 1 : 2
+        })
+        return response.data
     } catch (err) {
         throw err;
     }
