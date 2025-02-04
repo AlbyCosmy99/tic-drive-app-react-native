739f485e4f9a6637e7fa649f39802256b0f62dad
Andrei Albu <dinamo1999@icloud.com>
2025-02-04 23:07:50 +0100

    [FIX]

--- components/workshop/reviews/SeeAllReviewsCards.tsx
+++ components/workshop/reviews/SeeAllReviewsCards.tsx
@@ -14,17 +14,19 @@ const SeeAllReviewsCards = ({workshopId}: {workshopId: number}) => {
   return loadingReviews ? (
     <LoadingSpinner />
   ) : (
-    <View>
-      <ClientReviewCard review={reviews[0]} key={reviews[0].id} />
-      {reviews.length > 1 && (
-        <Pressable
-          className="border-2 border-grey-light items-center justify-center p-1 mx-2.5 my-0.5 rounded-xl"
-          onPress={handleOnSeeAllReviews}
-        >
-          <Text className="text-base font-medium">See all reviews</Text>
-        </Pressable>
-      )}
-    </View>
+    reviews.length && (
+      <View>
+        <ClientReviewCard review={reviews[0]} key={reviews[0].id} />
+        {reviews.length > 1 && (
+          <Pressable
+            className="border-2 border-grey-light items-center justify-center p-1 mx-2.5 my-0.5 rounded-xl"
+            onPress={handleOnSeeAllReviews}
+          >
+            <Text className="text-base font-medium">See all reviews</Text>
+          </Pressable>
+        )}
+      </View>
+    )
   );
 };
 
