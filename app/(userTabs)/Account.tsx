ee3ca82b230c33de60e9f7a5573252df4426dc74
Andrei Albu <dinamo1999@icloud.com>
2025-02-13 00:12:18 +0100

    fix

--- app/(userTabs)/Account.tsx
+++ app/(userTabs)/Account.tsx
@@ -10,110 +10,115 @@ import IconTextPair from '@/components/ui/IconTextPair';
 import TicDriveAuthButton from '@/components/ui/buttons/TicDriveAuthButton';
 import useJwtToken from '@/hooks/auth/useJwtToken';
 import NotLogged from '@/components/auth/NotLogged';
+
 export default function UserAccount() {
   const user = useAppSelector(state => state.auth.user);
   const token = useJwtToken();
-
-  if (!token) {
-    return <NotLogged />;
-  }
 
   return (
     <LinearGradientViewLayout>
       <SafeAreaViewLayout tailwindCss="mx-3.5">
         <TicDriveNavbar isLoginAvailable={false} />
-        <View className="flex flex-row items-center mb-4 mt-1">
-          <CircularUserAvatar
-            styles={{
-              width: 70,
-              height: 70,
-              marginRight: 10,
-            }}
-            uri="https://images.ladbible.com/resize?type=webp&quality=70&width=3840&fit=contain&gravity=auto&url=https://images.ladbiblegroup.com/v3/assets/bltb5d92757ac1ee045/bltc86e7943bcc0e006/6569cbef0b642304079a348b/AI-creates-what-the-average-person.png%3Fcrop%3D590%2C590%2Cx0%2Cy0"
-          />
-          <View>
-            <Text className="font-semibold text-xl">{user?.name}</Text>
-            <Text className="text-tic">New York, USA</Text>
-          </View>
-        </View>
-        <HorizontalLine />
-        <ScrollView>
-          <View className="my-4">
-            <Text className="font-medium text-2xl">Account</Text>
-            <View>
-              <IconTextPair
-                text="Phone number"
-                icon={<AddIcon />}
-                textTailwindCss="text-base font-medium"
-                containerTailwindCss="gap-1 py-1"
+        {token ? (
+          <>
+            <View className="flex flex-row items-center mb-4 mt-1">
+              <CircularUserAvatar
+                styles={{
+                  width: 70,
+                  height: 70,
+                  marginRight: 10,
+                }}
+                uri="https://images.ladbible.com/resize?type=webp&quality=70&width=3840&fit=contain&gravity=auto&url=https://images.ladbiblegroup.com/v3/assets/bltb5d92757ac1ee045/bltc86e7943bcc0e006/6569cbef0b642304079a348b/AI-creates-what-the-average-person.png%3Fcrop%3D590%2C590%2Cx0%2Cy0"
               />
-              <IconTextPair
-                text="Email"
-                icon={<AddIcon />}
-                textTailwindCss="text-base font-medium"
-                containerTailwindCss="gap-1 py-1"
-              />
-              <IconTextPair
-                text="Address"
-                icon={<AddIcon />}
-                textTailwindCss="text-base font-medium"
-                containerTailwindCss="gap-1 py-1"
-              />
-              <IconTextPair
-                text="Registered vehicles"
-                icon={<AddIcon />}
-                textTailwindCss="text-base font-medium"
-                containerTailwindCss="gap-1 py-1"
-              />
-              <IconTextPair
-                text="Favorite workshops"
-                icon={<AddIcon />}
-                textTailwindCss="text-base font-medium"
-                containerTailwindCss="gap-1 py-1"
-              />
+              <View>
+                <Text className="font-semibold text-xl">{user?.name}</Text>
+                <Text className="text-tic">New York, USA</Text>
+              </View>
             </View>
-          </View>
-          <HorizontalLine />
-          <View className="my-4">
-            <Text className="font-medium text-2xl">Payment information</Text>
-            <View>
-              <IconTextPair
-                text="Payment method"
-                icon={<AddIcon />}
-                textTailwindCss="text-base font-medium"
-                containerTailwindCss="gap-1 pt-2 pb-1"
-              />
-              <IconTextPair
-                text="Receipts"
-                icon={<AddIcon />}
-                textTailwindCss="text-base font-medium"
-                containerTailwindCss="gap-1 py-1"
-              />
+            <HorizontalLine />
+            <ScrollView>
+              <View className="my-4">
+                <Text className="font-medium text-2xl">Account</Text>
+                <View>
+                  <IconTextPair
+                    text="Phone number"
+                    icon={<AddIcon />}
+                    textTailwindCss="text-base font-medium"
+                    containerTailwindCss="gap-1 py-1"
+                  />
+                  <IconTextPair
+                    text="Email"
+                    icon={<AddIcon />}
+                    textTailwindCss="text-base font-medium"
+                    containerTailwindCss="gap-1 py-1"
+                  />
+                  <IconTextPair
+                    text="Address"
+                    icon={<AddIcon />}
+                    textTailwindCss="text-base font-medium"
+                    containerTailwindCss="gap-1 py-1"
+                  />
+                  <IconTextPair
+                    text="Registered vehicles"
+                    icon={<AddIcon />}
+                    textTailwindCss="text-base font-medium"
+                    containerTailwindCss="gap-1 py-1"
+                  />
+                  <IconTextPair
+                    text="Favorite workshops"
+                    icon={<AddIcon />}
+                    textTailwindCss="text-base font-medium"
+                    containerTailwindCss="gap-1 py-1"
+                  />
+                </View>
+              </View>
+              <HorizontalLine />
+              <View className="my-4">
+                <Text className="font-medium text-2xl">
+                  Payment information
+                </Text>
+                <View>
+                  <IconTextPair
+                    text="Payment method"
+                    icon={<AddIcon />}
+                    textTailwindCss="text-base font-medium"
+                    containerTailwindCss="gap-1 pt-2 pb-1"
+                  />
+                  <IconTextPair
+                    text="Receipts"
+                    icon={<AddIcon />}
+                    textTailwindCss="text-base font-medium"
+                    containerTailwindCss="gap-1 py-1"
+                  />
+                </View>
+              </View>
+              <HorizontalLine />
+              <View className="my-4">
+                <Text className="font-medium text-2xl">Help and support</Text>
+                <View>
+                  <IconTextPair
+                    text="Customer support"
+                    icon={<AddIcon />}
+                    textTailwindCss="text-base font-medium"
+                    containerTailwindCss="gap-1 pt-2 pb-1"
+                  />
+                  <IconTextPair
+                    text="Security information and resources"
+                    icon={<AddIcon />}
+                    textTailwindCss="text-base font-medium"
+                    containerTailwindCss="gap-1 py-1"
+                  />
+                </View>
+              </View>
+              <HorizontalLine />
+            </ScrollView>
+            <View className="my-2">
+              <TicDriveAuthButton action="logout" />
             </View>
-          </View>
-          <HorizontalLine />
-          <View className="my-4">
-            <Text className="font-medium text-2xl">Help and support</Text>
-            <View>
-              <IconTextPair
-                text="Customer support"
-                icon={<AddIcon />}
-                textTailwindCss="text-base font-medium"
-                containerTailwindCss="gap-1 pt-2 pb-1"
-              />
-              <IconTextPair
-                text="Security information and resources"
-                icon={<AddIcon />}
-                textTailwindCss="text-base font-medium"
-                containerTailwindCss="gap-1 py-1"
-              />
-            </View>
-          </View>
-          <HorizontalLine />
-        </ScrollView>
-        <View className="my-2">
-          <TicDriveAuthButton action="logout" />
-        </View>
+          </>
+        ) : (
+          <NotLogged />
+        )}
       </SafeAreaViewLayout>
     </LinearGradientViewLayout>
   );
--- app/(userTabs)/Bookings.tsx
+++ app/(userTabs)/Bookings.tsx
@@ -1,16 +1,25 @@
 import NotLogged from '@/components/auth/NotLogged';
 import useJwtToken from '@/hooks/auth/useJwtToken';
 import {View, Text} from 'react-native';
+import LinearGradientViewLayout from '../layouts/LinearGradientViewLayout';
+import SafeAreaViewLayout from '../layouts/SafeAreaViewLayout';
+import TicDriveNavbar from '@/components/navigation/TicDriveNavbar';
 
 export default function UserBookings() {
   const token = useJwtToken();
 
-  if (!token) {
-    return <NotLogged />;
-  }
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text>Bookings</Text>
-    </View>
+    <LinearGradientViewLayout>
+      <SafeAreaViewLayout tailwindCss="mx-3.5">
+        <TicDriveNavbar isLoginAvailable={false} />
+        {token ? (
+          <View>
+            <Text>bookings</Text>
+          </View>
+        ) : (
+          <NotLogged />
+        )}
+      </SafeAreaViewLayout>
+    </LinearGradientViewLayout>
   );
 }
