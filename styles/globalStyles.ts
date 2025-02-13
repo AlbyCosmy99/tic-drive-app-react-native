d0c544c5ebfc648e714a0150a725f6d041b90f2d
Andrei Albu <dinamo1999@icloud.com>
2025-02-13 01:08:46 +0100

    fix: globalStyles on android

--- styles/globalStyles.ts
+++ styles/globalStyles.ts
@@ -1,12 +1,13 @@
-import {StatusBar, StyleSheet, Platform} from 'react-native';
-import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import { StatusBar, StyleSheet, Platform } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export const globalStyles = () => {
   const insets = useSafeAreaInsets();
 
   return StyleSheet.create({
     safeAreaView: {
-      paddingTop: Platform.OS === 'ios' ? insets.top : StatusBar.currentHeight,
+      paddingTop: Platform.OS === 'ios' ? 0 : StatusBar.currentHeight,
+      ...(Platform.OS !== 'ios' && { backgroundColor: 'white' }),
     },
   });
 };
