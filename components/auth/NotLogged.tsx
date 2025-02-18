25f274d3f912c0a3293be7f451efb3cc9266af2a
Andrei Albu <dinamo1999@icloud.com>
2025-02-18 23:48:29 +0100

    fix: adjust bug on NotLogged component

--- components/auth/NotLogged.tsx
+++ components/auth/NotLogged.tsx
@@ -1,18 +1,18 @@
 import LinearGradientViewLayout from '@/app/layouts/LinearGradientViewLayout';
 import SafeAreaViewLayout from '@/app/layouts/SafeAreaViewLayout';
-import {Text} from 'react-native';
+import {Text, View} from 'react-native';
 import TicDriveAuthButton from '../ui/buttons/TicDriveAuthButton';
 
 const NotLogged = () => {
   return (
     <LinearGradientViewLayout>
-      <SafeAreaViewLayout tailwindCss="items-center justify-center">
+      <View className="items-center justify-center flex-1">
         <Text className="font-medium text-lg mb-2">You are not logged in.</Text>
         <Text className="font-medium text-md mb-4">
           Login or register using the button below.
         </Text>
         <TicDriveAuthButton action="login" />
-      </SafeAreaViewLayout>
+      </View>
     </LinearGradientViewLayout>
   );
 };
