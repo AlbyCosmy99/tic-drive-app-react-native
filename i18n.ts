05440f9b3b3c8392b41e650a1ed16134db3ec606
Andrei Albu <dinamo1999@icloud.com>
2025-02-27 00:38:31 +0100

    format with prettier

--- i18n.ts
+++ i18n.ts
@@ -1,25 +1,23 @@
 import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+import {initReactI18next} from 'react-i18next';
 
 import en from './locales/en.json';
 import it from './locales/it.json';
 
 const resources = {
-  en: { translation: en },
-  it: { translation: it },
+  en: {translation: en},
+  it: {translation: it},
 };
 
-const languageCode = 'it' //todo: make the language dynamic
+const languageCode = 'it'; //todo: make the language dynamic
 
-i18n
-  .use(initReactI18next)
-  .init({
-    resources,
-    lng: languageCode,
-    fallbackLng: 'en', 
-    interpolation: {
-      escapeValue: false,
-    },
-  });
+i18n.use(initReactI18next).init({
+  resources,
+  lng: languageCode,
+  fallbackLng: 'en',
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
 export default i18n;
