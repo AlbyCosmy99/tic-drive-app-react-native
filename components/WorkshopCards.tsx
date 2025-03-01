42c601b1c47a6fa8024eb94de6a9b76b5d3ba6cd
Andrei Albu <dinamo1999@icloud.com>
2025-03-02 00:40:57 +0100

    fix: infinite scrolling for workshops

--- components/WorkshopCards.tsx
+++ components/WorkshopCards.tsx
@@ -1,22 +1,20 @@
+import React, { memo, useContext, useEffect, useMemo, useRef, useState } from 'react';
+import { ActivityIndicator, Text, TouchableOpacity, View, FlatList } from 'react-native';
 import WorkshopCard from './WorkshopCard';
-import {ActivityIndicator, Text, TouchableOpacity} from 'react-native';
-import {memo, useContext, useEffect, useMemo, useState} from 'react';
 import GlobalContext from '@/stateManagement/contexts/global/GlobalContext';
 import navigationPush from '@/services/navigation/push';
-import {useServicesChoosenByUsers} from '@/hooks/user/useServiceChoosenByUsers';
+import { useServicesChoosenByUsers } from '@/hooks/user/useServiceChoosenByUsers';
 import useJwtToken from '@/hooks/auth/useJwtToken';
 import useAreServicesAvailable from '@/hooks/services/useAreServicesAvailable';
 import useWorkshops from '@/hooks/api/workshops/useWorkshops';
 import LoadingSpinner from './ui/loading/LoadingSpinner';
-import {View} from 'react-native';
 import TicDriveButton from './ui/buttons/TicDriveButton';
 import navigationReset from '@/services/navigation/reset';
-import {reset} from '@/stateManagement/redux/slices/servicesSlice';
-import {useAppDispatch} from '@/stateManagement/redux/hooks';
-import {useFocusEffect} from 'expo-router';
-import {setSelectedWorkshop} from '@/stateManagement/redux/slices/workshopsSlice';
+import { reset } from '@/stateManagement/redux/slices/servicesSlice';
+import { useAppDispatch } from '@/stateManagement/redux/hooks';
+import { useFocusEffect } from 'expo-router';
+import { setSelectedWorkshop } from '@/stateManagement/redux/slices/workshopsSlice';
 import Workshop from '@/types/workshops/Workshop';
-import {FlatList} from 'react-native-gesture-handler';
 import useTicDriveNavigation from '@/hooks/navigation/useTicDriveNavigation';
 
 interface WorkshopCardsProps {
@@ -30,28 +28,26 @@ const WorkshopCards: React.FC<WorkshopCardsProps> = ({
   setAreNoWorkshop = () => {},
   favorite = false,
 }) => {
-  const {workshopFilter, setWorkshopFilter} = useContext(GlobalContext);
+  const { workshopFilter, setWorkshopFilter } = useContext(GlobalContext);
   const navigation = useTicDriveNavigation();
 
   const servicesChoosen = useServicesChoosenByUsers();
-  const {areServicesAvailable} = useAreServicesAvailable();
+  const { areServicesAvailable } = useAreServicesAvailable();
   const [currentPage, setCurrentPage] = useState(1);
-
   const workshopsPerPage = 10;
 
-  const {workshops, loadingWorkshops, setLoadingWorkshops, count} =
-    useWorkshops(
-      (currentPage - 1) * workshopsPerPage,
-      workshopsPerPage,
-      areServicesAvailable ? servicesChoosen[0]?.id : 0,
-      true,
-      favorite,
-    );
+  const { workshops, loadingWorkshops, setLoadingWorkshops, count } = useWorkshops(
+    (currentPage - 1) * workshopsPerPage,
+    workshopsPerPage,
+    areServicesAvailable ? servicesChoosen[0]?.id : 0,
+    true,
+    favorite,
+  );
 
   const [accWorkshops, setAccWorkshops] = useState<Workshop[]>([]);
-
   const token = useJwtToken();
   const dispatch = useAppDispatch();
+
   const handleChooseDifferentService = () => {
     dispatch(reset());
     navigationPush(navigation, 'ChooseServicesScreen');
@@ -62,6 +58,7 @@ const WorkshopCards: React.FC<WorkshopCardsProps> = ({
       if (currentPage === 1) {
         setAccWorkshops(workshops);
       } else {
+        console.log(currentPage);
         setAccWorkshops(prev => [...prev, ...workshops]);
       }
     }
@@ -69,15 +66,11 @@ const WorkshopCards: React.FC<WorkshopCardsProps> = ({
 
   const filteredWorkshops = useMemo(
     () =>
-      accWorkshops.filter(
-        workshop =>
-          workshopFilter.length === 0 ||
-          workshop.name
-            ?.toLowerCase()
-            .trim()
-            .includes(workshopFilter.toLowerCase().trim()),
+      accWorkshops.filter(workshop =>
+        workshopFilter.length === 0 ||
+        workshop.name?.toLowerCase().trim().includes(workshopFilter.toLowerCase().trim())
       ),
-    [accWorkshops, workshopFilter],
+    [accWorkshops, workshopFilter]
   );
 
   useEffect(() => {
@@ -95,24 +88,19 @@ const WorkshopCards: React.FC<WorkshopCardsProps> = ({
   });
 
   const handleCardPress = (workshop: Workshop) => {
-    navigationPush(navigation, 'WorkshopDetails', {workshop});
+    navigationPush(navigation, 'WorkshopDetails', { workshop });
   };
 
-  const pages = Math.ceil(20 / workshopsPerPage);
+  const pages = Math.ceil(count / workshopsPerPage);
+  const onEndReachedCalledDuringMomentum = useRef(false);
 
   return filteredWorkshops.length === 0 && !loadingWorkshops ? (
     <View className="flex-1 justify-center items-center mx-2.5">
       <Text className="text-lg text-gray-600 text-center">
         No workshop found. Try with a different service, or go to the dashboard.
       </Text>
-      <TicDriveButton
-        text="Look for a different service"
-        onClick={handleChooseDifferentService}
-      />
-      <TicDriveButton
-        text="Go back to dashboard"
-        onClick={() => navigationReset(navigation, 0, 'Hub', 'Home')}
-      />
+      <TicDriveButton text="Look for a different service" onClick={handleChooseDifferentService} />
+      <TicDriveButton text="Go back to dashboard" onClick={() => navigationReset(navigation, 0, 'Hub', 'Home')} />
     </View>
   ) : (
     <View className={`${!token ? 'mb-2' : ''} ${tailwindContainerCss}`}>
@@ -124,25 +112,25 @@ const WorkshopCards: React.FC<WorkshopCardsProps> = ({
         <FlatList
           data={filteredWorkshops}
           keyExtractor={item => item.id.toString()}
-          renderItem={({item}) => (
+          renderItem={({ item }) => (
             <TouchableOpacity onPress={() => handleCardPress(item)}>
               <WorkshopCard workshop={item} />
             </TouchableOpacity>
           )}
+          onMomentumScrollBegin={() => {
+            onEndReachedCalledDuringMomentum.current = false;
+          }}
           onEndReached={() => {
-            if (currentPage < pages) {
-              setCurrentPage(currentPage + 1);
+            if (!onEndReachedCalledDuringMomentum.current && !loadingWorkshops && currentPage < pages) {
+              onEndReachedCalledDuringMomentum.current = true;
+              setCurrentPage(prevPage => prevPage + 1);
               setLoadingWorkshops(true);
             }
           }}
           onEndReachedThreshold={0.5}
           ListFooterComponent={() =>
             loadingWorkshops && currentPage < pages ? (
-              <ActivityIndicator
-                size="large"
-                color="green"
-                style={{margin: 20}}
-              />
+              <ActivityIndicator size="large" color="green" style={{ margin: 20 }} />
             ) : null
           }
         />
