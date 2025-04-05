6fd5556f59a6d6cd876152737a4d4d8f89bbd7dd
Andrei Albu <dinamo1999@icloud.com>
2025-04-05 23:28:59 +0200

    format

--- utils/car/isPlateNumber.ts
+++ utils/car/isPlateNumber.ts
@@ -1,5 +1,5 @@
 const isPlateNumber = (plate: string, country: string = 'IT'): boolean => {
-  const platePatterns: { [key: string]: RegExp } = {
+  const platePatterns: {[key: string]: RegExp} = {
     IT: /^[A-Z]{2}\d{3}[A-Z]{2}$/, // Italian plates: AB123CD
     FR: /^[A-Z]{2}-\d{3}-[A-Z]{2}$/, // French plates: AB-123-CD
     DE: /^[A-Z]{1,3}-[A-Z]{1,2}-\d{1,4}$/, // German plates: B-AB-1234
@@ -11,4 +11,4 @@ const isPlateNumber = (plate: string, country: string = 'IT'): boolean => {
   return regex ? regex.test(plate?.toUpperCase()) : false;
 };
 
-export default isPlateNumber
+export default isPlateNumber;
