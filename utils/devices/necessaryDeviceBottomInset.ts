c70eab7bdad77d0718e46af88ec34c95cbe21b4a
Andrei Albu <dinamo1999@icloud.com>
2025-04-12 11:29:30 +0200

    fix: remove comment

--- utils/devices/necessaryDeviceBottomInset.ts
+++ utils/devices/necessaryDeviceBottomInset.ts
@@ -1,7 +1,6 @@
 import doesDeviceNeedsBottomInset from './doesDeviceNeedsBottomInset';
 
 const necessaryDeviceBottomInset = () => {
-  //tailwind css
   return doesDeviceNeedsBottomInset() ? 'mb-4' : '';
 };
 
