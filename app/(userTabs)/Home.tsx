fe0ab02dedeb26ccab5f13493e4274e78ea91fef
HilinaBI <HilinaBizuneh670@gmail.com>
2025-04-15 16:14:01 +0200

    add location on user home

--- app/(userTabs)/Home.tsx
+++ app/(userTabs)/Home.tsx
@@ -1,43 +1,44 @@
-import {Pressable, Text, View} from 'react-native';
+import PinLocationIcon from '@/assets/svg/location/PinLocation.svg';
+import NissanIcon from '@/assets/svg/vehicles/makes/nissan.svg';
+import PeugeotIcon from '@/assets/svg/vehicles/makes/peugeot.svg';
+import FilterSearchModal from '@/components/modal/FilterSearchModal';
 import TicDriveNavbar from '@/components/navigation/TicDriveNavbar';
+import SeeAllServicesCards from '@/components/services/SeeAllServicesCards';
+import CrossPlatformButtonLayout from '@/components/ui/buttons/CrossPlatformButtonLayout';
+import TicDriveReminderCard from '@/components/ui/cards/notifications/TicDriveReminderCard';
+import HorizontalLine from '@/components/ui/HorizontalLine';
 import TicDriveInput from '@/components/ui/inputs/TicDriveInput';
-import {useCallback, useContext, useRef, useState} from 'react';
-import debounce from 'lodash.debounce';
-import navigationPush from '@/services/navigation/push';
-import SafeAreaViewLayout from '../layouts/SafeAreaViewLayout';
-import LinearGradientViewLayout from '../layouts/LinearGradientViewLayout';
+import LoadingSpinner from '@/components/ui/loading/LoadingSpinner';
+import ErrorModal from '@/components/ui/modals/ErrorModal';
 import WorkshopCardMini from '@/components/workshop/WorkshopCardMini';
-import {useAppDispatch, useAppSelector} from '@/stateManagement/redux/hooks';
+import { Colors } from '@/constants/Colors';
+import useWorkshops from '@/hooks/api/workshops/useWorkshops';
+import useJwtToken from '@/hooks/auth/useJwtToken';
+import useTicDriveNavigation from '@/hooks/navigation/useTicDriveNavigation';
+import axiosClient from '@/services/http/axiosClient';
+import navigationPush from '@/services/navigation/push';
+import CarContext from '@/stateManagement/contexts/car/CarContext';
+import { useAppDispatch, useAppSelector } from '@/stateManagement/redux/hooks';
+import { setSelectedCar } from '@/stateManagement/redux/slices/carsSlice';
 import {
   reset,
   setAreServicesOn,
 } from '@/stateManagement/redux/slices/servicesSlice';
-import {useFocusEffect} from '@react-navigation/native';
-import {RefreshControl, ScrollView} from 'react-native-gesture-handler';
-import LoadingSpinner from '@/components/ui/loading/LoadingSpinner';
-import useWorkshops from '@/hooks/api/workshops/useWorkshops';
-import useJwtToken from '@/hooks/auth/useJwtToken';
-import {Colors} from '@/constants/Colors';
-import HorizontalLine from '@/components/ui/HorizontalLine';
 import {
   setLastWorkshopSelectedFromFilter,
   setSelectedWorkshop,
 } from '@/stateManagement/redux/slices/workshopsSlice';
-import SeeAllServicesCards from '@/components/services/SeeAllServicesCards';
-import CrossPlatformButtonLayout from '@/components/ui/buttons/CrossPlatformButtonLayout';
-import useTicDriveNavigation from '@/hooks/navigation/useTicDriveNavigation';
-import {Entypo} from '@expo/vector-icons';
-import isAndroidPlatform from '@/utils/devices/isAndroidPlatform';
-import TicDriveReminderCard from '@/components/ui/cards/notifications/TicDriveReminderCard';
-import NissanIcon from '@/assets/svg/vehicles/makes/nissan.svg';
-import PeugeotIcon from '@/assets/svg/vehicles/makes/peugeot.svg';
-import {useTranslation} from 'react-i18next';
-import axiosClient from '@/services/http/axiosClient';
 import Workshop from '@/types/workshops/Workshop';
-import FilterSearchModal from '@/components/modal/FilterSearchModal';
-import ErrorModal from '@/components/ui/modals/ErrorModal';
-import {setSelectedCar} from '@/stateManagement/redux/slices/carsSlice';
-import CarContext from '@/stateManagement/contexts/car/CarContext';
+import isAndroidPlatform from '@/utils/devices/isAndroidPlatform';
+import { Entypo } from '@expo/vector-icons';
+import { useFocusEffect } from '@react-navigation/native';
+import debounce from 'lodash.debounce';
+import { useCallback, useContext, useRef, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Text, View } from 'react-native';
+import { RefreshControl, ScrollView } from 'react-native-gesture-handler';
+import LinearGradientViewLayout from '../layouts/LinearGradientViewLayout';
+import SafeAreaViewLayout from '../layouts/SafeAreaViewLayout';
 
 export default function UserHome() {
   const [filter, setFilter] = useState('');
@@ -58,6 +59,7 @@ export default function UserHome() {
   const handleOnSeeAllWorkshops = () => {
     navigationPush(navigation, 'WorkshopsListScreen');
   };
+  const userAddress = useAppSelector(state => state.auth.user?.address) ?? '';
 
   const handleVehicles = () => {
     token
@@ -125,6 +127,17 @@ export default function UserHome() {
             });
           }}
         />
+         <View className="flex-row items-center px-4 pt-1 pb-1">
+          <PinLocationIcon width={16} height={16} />
+           <Text
+           className="ml-1 text-sm font-medium text-gray-700"
+           numberOfLines={1}
+           ellipsizeMode="tail"
+          >
+         {userAddress}
+           </Text>
+        </View>
+
         <View className="flex-row items-center relative">
           <TicDriveInput
             isLeftIcon={true}
