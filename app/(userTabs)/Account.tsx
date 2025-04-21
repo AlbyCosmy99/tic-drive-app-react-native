5e1bc0b03b535a54acb2e2f2c439f8d7de9979c2
HilinaBI <HilinaBizuneh670@gmail.com>
2025-04-21 22:44:59 +0200

    userprofile layout fixed

--- app/(userTabs)/Account.tsx
+++ app/(userTabs)/Account.tsx
@@ -7,7 +7,6 @@ import {
   Alert,
 } from 'react-native';
 import React, { useState, useEffect } from 'react';
-
 // Navigation & State
 import useTicDriveNavigation from '@/hooks/navigation/useTicDriveNavigation';
 import navigationPush from '@/services/navigation/push';
