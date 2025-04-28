dd996202ce3aedcb821e16bb15be0683c6d6ea15
Andrei <albu.cosminandrei.1999@gmail.com>
2025-04-28 21:07:18 +0200

    fix: solve bug on workshops reviews

--- components/workshop/reviews/WorkshopReviewInfo.tsx
+++ components/workshop/reviews/WorkshopReviewInfo.tsx
@@ -1,8 +1,8 @@
 import Star from '../../../assets/svg/star.svg';
 import IconTextPair from '@/components/ui/IconTextPair';
 import Workshop from '@/types/workshops/Workshop';
-import {useMemo} from 'react';
-import {useTranslation} from 'react-i18next';
+import { useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
 
 interface WorkshopReviewinfoProps {
   meanStars?: Workshop['meanStars'];
@@ -17,14 +17,16 @@ const WorkshopReviewinfo: React.FC<WorkshopReviewinfoProps> = ({
   textTailwindCss,
 }) => {
   const maxGrade = 5;
-  const {t} = useTranslation();
+  const { t } = useTranslation();
 
   const text = useMemo(() => {
     if (numberOfReviews && numberOfReviews > 0) {
-      return `${meanStars}/${maxGrade} (${numberOfReviews} ${numberOfReviews !== 1 ? t('workshops.reviews.title') : t('workshops.reviews.review')})`;
+      const roundedMeanStars = meanStars ? meanStars.toFixed(1) : '0.0';
+      return `${roundedMeanStars}/${maxGrade} (${numberOfReviews} ${numberOfReviews !== 1 ? t('workshops.reviews.title') : t('workshops.reviews.review')})`;
     }
     return `${numberOfReviews} ${numberOfReviews !== 1 ? t('workshops.reviews.title') : t('workshops.reviews.review')}`;
   }, [numberOfReviews, meanStars]);
+
 
   return (
     (numberOfReviews || numberOfReviews === 0) && (
