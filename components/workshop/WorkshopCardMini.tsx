c371af290c2149b6f5b3d1de08a7b3eb5f553ce6
albycosmy99 <dinamo1999@icloud.com>
2025-05-16 23:38:06 +0200

    fix

--- components/workshop/WorkshopCardMini.tsx
+++ components/workshop/WorkshopCardMini.tsx
@@ -9,7 +9,7 @@ function WorkshopCardMini({workshop}: {workshop: Workshop}) {
       pressableContainerStyle={{padding: 0, paddingHorizontal: 2}}
       iconTextPairsContainerTailwindCss="px-0.5 pb-0 pt-0.5 justify-between h-24 mx-0.5 my-0.5"
       iconTextPairTextTailwindCss="text-[11px] mr-1"
-      iconTextPairContainerTailwindCss="gap-1 py-0.5 min-h-14 w-40 pr-0.5"
+      iconTextPairContainerTailwindCss="gap-1 py-0.5 min-h-14 pr-0.5"
       imageContainerStyle={{height: 80}}
       viewContainerStyle={{height: 190}}
       isServiceDetailsEnabled={false}
