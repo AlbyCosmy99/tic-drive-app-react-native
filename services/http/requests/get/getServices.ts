d9df81197c6ce92599d8d18416d451c92f14e82a
albycosmy99 <dinamo1999@icloud.com>
2025-05-28 00:04:31 +0200

    services api

--- services/http/requests/get/getServices.ts
+++ services/http/requests/get/getServices.ts
@@ -1,13 +1,21 @@
 import axiosClient from '../../axiosClient';
 
-const getServices = async (workshopId: string, languageCode: string) => {
-  const res = await axiosClient.get(
-    'services?workshopId=' +
-      (workshopId ? workshopId : '') +
-      '&languageCode=' +
-      languageCode +
-      '&take=1000',
-  );
+const getServices = async (
+  workshopId: string,
+  languageCode: string,
+  fatherId?: number
+) => {
+  const queryParams = new URLSearchParams({
+    workshopId: workshopId ?? '',
+    languageCode,
+    take: '1000',
+  });
+
+  if (fatherId !== undefined) {
+    queryParams.append('fatherId', fatherId.toString());
+  }
+
+  const res = await axiosClient.get(`services?${queryParams.toString()}`);
   return res.data;
 };
 
