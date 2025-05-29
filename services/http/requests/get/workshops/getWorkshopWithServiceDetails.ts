8733bf966fabb92a64674e99774e107852ca5dff
albycosmy99 <dinamo1999@icloud.com>
2025-05-29 18:04:45 +0200

    feat: improve booking page

---
+++ services/http/requests/get/workshops/getWorkshopWithServiceDetails.ts
@@ -0,0 +1,7 @@
+import axiosClient from "@/services/http/axiosClient"
+
+const getWorkshopWithServiceDetails = (workshopId: string, serviceId: number) => {
+    return axiosClient.get('workshops?workshopId=' + workshopId + '&serviceId=' + serviceId)
+}
+
+export default getWorkshopWithServiceDetails
