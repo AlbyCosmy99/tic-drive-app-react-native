fb2a6323cbe175052fc9f68f90468e17eccd2f78
albycosmy99 <dinamo1999@icloud.com>
2025-06-07 21:41:58 +0200

    fix

--- components/ui/buttons/OAuth2Button.tsx
+++ components/ui/buttons/OAuth2Button.tsx
@@ -1,20 +1,23 @@
-import {Colors} from '@/constants/Colors';
-import {Button} from '@rneui/themed';
-import React, {ReactNode} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import { Colors } from '@/constants/Colors';
+import { Button } from '@rneui/themed';
+import React, { ReactNode } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
 interface OAuth2ButtonProps {
   text: string;
   icon: ReactNode;
+  onPress: () => void;
 }
 
-const OAuth2Button: React.FC<OAuth2ButtonProps> = ({text, icon}) => {
+const OAuth2Button: React.FC<OAuth2ButtonProps> = ({ text, icon, onPress }) => {
   return (
     <View className="flex-1 m-1 mx-2">
-      <Button buttonStyle={styles.button}>
-        <View className="flex-row items-center justify-center gap-1 p-1">
+      <Button buttonStyle={styles.button} onPress={onPress}>
+        <View className="flex-row items-center justify-center gap-1 py-1 px-2">
           {icon}
-          <Text className="text-lg">{text}</Text>
+          <Text allowFontScaling={false} className="text-lg text-black">
+            {text}
+          </Text>
         </View>
       </Button>
     </View>
@@ -27,6 +30,8 @@ const styles = StyleSheet.create({
     borderRadius: 40,
     borderWidth: 1,
     borderColor: Colors.light.OAuth2BorderButton,
+    paddingVertical: 8,
+    paddingHorizontal: 12,
   },
 });
 
